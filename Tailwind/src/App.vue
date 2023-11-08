ea2dbb1c6476b4a77c96c416356d16ebd84d2860
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-08 17:20:20 +0545

    Tailwind

---
+++ Tailwind/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Tailwind from './components/Tailwind.vue';
+
+</script>
+
+<template>
+ <Tailwind/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Tailwind/src/components/Tailwind.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <nav class="mt-12">
+    <ul class="flex px-4 py-4 bg-red-500 text-white " >
+        <li class="mr-4  cursor-pointer">Home</li>
+        <li class="mr-4  cursor-pointer">About</li>
+        <li class="mr-4  cursor-pointer">Services</li>
+    </ul>
+  </nav>
+</template>
